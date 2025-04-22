90a348e30df5b4a3fa796cbc212f1e2e73979e7f
ange <ange@yw5n.com>
2025-04-22 05:00:13 +0000

    aaa

--- flutter.sh
+++ flutter.sh
@@ -3,5 +3,8 @@
 IMG=git.gmoker.com/icing/flutter:main
 
 docker volume create --ignore flutter_cache
+docker volume create --ignore flutter_cache1
 set -x
-docker run -it --network host --rm -v "flutter_cache:/root/" -v "$PWD:/app/" --entrypoint flutter "$IMG" --no-version-check "$@"
+docker run -it --rm -v "flutter_cache:/root/" \
+    -v "flutter_cache1:/opt/android-sdk/" \
+    -v "/dev/bus/usb/:/dev/bus/usb/" -v "$PWD:/app/" "$IMG" "$@"
--- lib/main.dart
+++ lib/main.dart
@@ -1,52 +1,67 @@
 import 'package:flutter/material.dart';
 
-void main() {
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+class Story {
+  final String title;
+  final String description;
+  final String image;
+
+  const Story({required this.title, required this.description, required this.image});
+
+  Map<String, Object?> toMap() {
+    return {
+      'title': title, 'description': description, 'image': image
+    };
+  }
+
+  @override
+  String toString() {
+    return 'Story{title: $title, description: $description, image: $image}';
+  }
+}
+
+Future<Database> opendb() async {
+  return openDatabase(
+    join(await getDatabasesPath(), 'stories.db'),
+    onCreate: (db, version) {
+      return db.execute(
+        'CREATE TABLE stories(id INTEGER PRIMARY KEY, title VARCHAR(255), description TEXT, image VARCHAR(1024))',
+      );
+    },
+    version: 1,
+  );
+}
+
+Future<void> insertStory(Story story) async {
+  final db = await opendb();
+
+  await db.insert('stories', story.toMap());
+}
+
+void main() async {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'StoryBase',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'StoryBase'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -55,71 +70,55 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  @override
+  List<Widget> _stories = [];
 
-  void _incrementCounter() {
+  void _addEntry() async {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      var s = Story(title: 'title', description: '', image: '');
+
+      _stories.add(_container(s.title));
+      insertStory(s);
     });
   }
 
-  @override
+  Container _container(String title) {
+    return Container(
+          height: 64,
+          child: Text(title),
+    );
+  }
+
+  void _getStories() {
+    opendb().then((db) {
+      db.query('stories').then((List<Map<String, dynamic>> stories) {
+
+        for (var s in stories) {
+          _stories.add(_container(s['title']));
+        }
+      });
+    });
+  }
+
+  void initState() {
+    _getStories();
+  }
+
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
         ),
+      body: ListView(
+        padding: const EdgeInsets.all(8),
+        children: _stories,
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        onPressed: _addEntry,
+        tooltip: 'add',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        ),
     );
   }
 }
